f41c85eca4fa7159f1cc0dd2ac62f4f627e68f9a
zhuqianye <q835041058@gmail.com>
2017-06-24 20:31:57 +0800

    web interface ok

--- app.js
+++ app.js
@@ -27,4 +27,14 @@ app.use(controller.get('/ejs_test',function*(){
 app.use(controller.get('/api_test',function*(){
 	this.body=service.get_test_data();
 }));
+app.use(controller.get('/ajax/search',function*(){
+	this.set('Cache-Control','no-cache');
+	var querystring=require('querystring');
+	var params=querystring.parse(this.req._parsedUrl.query);
+	// 把参数解析成object
+	var start=params.start;
+	var end=params.end;
+	var keyword=params.keyword;
+	this.body=yield service.get_search_data(start,end,keyword);
+}));
 app.listen(3000);
--- service/webAppService.js
+++ service/webAppService.js
@@ -1,7 +1,44 @@
 var fs=require('fs');
+var qs=require('querystring');
+var http=require('http');
 //文件系统，像python一样
 
 exports.get_test_data=function(){
 	var content=fs.readFileSync('./mock/test.json','utf-8');
 	return content;
+};
+
+exports.get_search_data=function(start,end,keyword){
+	return function(callback){
+		//object对象转成
+		//{a:'1'}（它就是object)，后端接受的是http://127.0.0.1/api?a=1
+		var data={
+			start:start,
+			end:end,
+			s:keyword
+		};
+		var content=qs.stringify(data);
+		console.log("查询的地址是:"+content);
+		var http_request={
+			hostname:'dushu.xiaomi.com',
+			port:80,
+			path:'/store/v0/lib/query/onebox?'+content
+		};
+
+		req_obj=http.request(http_request,function(_res){
+			var content='';
+			_res.setEncoding('utf-8');
+			_res.on('data',function(chunk){
+				content=content+chunk;
+				//拼接所有的数据
+			});//返回数据，触发data方法，注意：返回的是一部分数据
+			_res.on('end',function(){
+				callback(null,content);
+			});//end之后所有数据都返回了
+		});
+		req_obj.on('error',function(){
+
+		});
+		req_obj.end();//发送这个请求
+	};
 };
